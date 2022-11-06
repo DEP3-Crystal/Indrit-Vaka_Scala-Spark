6da3431dcbe2742878cbfe4f46b4827f06ede8c7
Indrit-Vaka-w <109148111+Indrit-Vaka-w@users.noreply.github.com>
2022-11-06 12:22:43 +0100

    Spark Key value pairs

---
+++ spark/src/main/scala/com.crystal/kv_paris/Car.scala
@@ -0,0 +1,5 @@
+package com.crystal.kv_paris
+
+//car,price,body,mileage,engV,engType,registration,year,model,drive
+case class Car(car: String, price: Double, body: String, mileage: String, engV: String,
+               engType: String, registration: String, year: Int, model: String, drive: String)
---
+++ spark/src/main/scala/com.crystal/kv_paris/Main.scala
@@ -0,0 +1,53 @@
+package com.crystal.kv_paris
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Main extends App {
+
+  val conf = new SparkConf()
+    .setAppName("Car_Stats")
+    .setMaster("local[*]") //* is number of cores
+
+  val root = "C:\\Users\\indri\\Desktop\\CrystalSystem\\Indrit-Vaka_Scala-Spark\\source\\car_ads"
+  val sc = SparkContext.getOrCreate(conf)
+  val header = "car,price,body,mileage,engV,engType,registration,year,model,drive"
+
+
+  sc.textFile(root + "/car_ads_*.csv")
+
+    .filter(!_.contains(header))
+    .filter(_.split(",").length >= 10)
+    .map(DeserializeCar.deserialize)
+    .map(car=>new ToKVByEntry[String, Car](key=>key.car).convert(car))
+    .groupByKey()
+    .map(kv=> {
+      val key = kv._1
+      val avg = kv._2.map(_.price).sum / kv._2.size
+      (key, avg)
+    })
+    .saveAsTextFile(root+"sink/car_ads")
+//    .foreach(println)
+
+
+}
+
+object DeserializeCar {
+  def deserialize: String => Car = line => {
+    val entries = line.split(",")
+    //car,price,body,mileage,engV,engType,registration,year,model,drive
+    Car(entries(0), entries(1).toDouble, entries(2), entries(3), entries(4), entries(5),
+      entries(6), entries(7).toInt, entries(8), entries(9))
+  }
+}
+
+class ToKVByEntry[K, V](keyProvider: KeyProvider[K,V]) {
+
+  def convert(value: V): (K, V) = {
+    (keyProvider.getKey(value), value)
+  }
+
+}
+
+trait KeyProvider[K, V] {
+  def getKey(v: V): K
+}
